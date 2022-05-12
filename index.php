213632404b81d73f4d657dda40689f27855838ed
Nowrei <furlancedric@gmail.com>
2022-05-12 15:50:14 +0200

    Petit changement

--- index.php
+++ index.php
@@ -18,13 +18,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://kit.fontawesome.com/ee02dbcf72.js" crossorigin="anonymous"></script>
-
-  
-
- 
-  
-
-  <title>Metalinov</title>
+<title>Metalinov</title>
 <body>
 <?php include("assets/include/navbarre.php") ?>
 
