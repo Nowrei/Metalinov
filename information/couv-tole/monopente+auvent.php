c5bd1c87d5d0683b686c709de05ee5deb212d0a0
Nowrei <furlancedric@gmail.com>
2022-05-17 14:29:41 +0200

    image gallrie mise en place

---
+++ information/couv-tole/monopente+auvent.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Couverture-tole</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="https:////cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="../../assets/styles/gallerie.css">
+    <link rel="stylesheet" href="../../assets/styles/footer.css">
+    <link rel="stylesheet" href="../../assets/styles/back.css">
+    <script src="https://kit.fontawesome.com/ee02dbcf72.js" crossorigin="anonymous"></script>
+  
+</head>
+<body>
+<?php include '../../assets/include/navbarre-couv.php' ?>
+<div class="w-100 p-3 text-dark">
+        
+        <h3 class="text-dark-50 me-5 ms-5 mt-4">
+          Modèles de bâtiments standards couveture tôle - Monopente + Auvent tôle
+        </h3>
+    </div>
+
+<div>
+<section id="gallery">
+  <div class="container">
+    <div id="image-gallery">
+      <div class="row">
+        <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12 mx-auto image">
+        <div class="card-body" style="text-align: center;">
+                    <h4 class="card-title mt-2 text-center text-secondary">Bi-pente de largeur 8.39m</h4>
+                </div>
+          <div class="img-wrapper">
+            <a href="../../assets/img/tole-fibro/zoom_bp08.gif"><img src="../../assets/img/tole-fibro/zoom_bp08.gif" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-plus-circle" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12 mx-auto image">
+        <div class="card-body" style="text-align: center;">
+                    <h4 class="card-title mt-2 text-center text-secondary">Bi-pente de largeur 12.26m</h4>
+                </div>
+          <div class="img-wrapper">
+            <a href="../../assets/img/tole-fibro/zoom_bp12.gif"><img src="../../assets/img/tole-fibro/zoom_bp12.gif" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-plus-circle" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12 mx-auto image">
+        <div class="card-body" style="text-align: center;">
+                    <h4 class="card-title mt-5 text-center text-secondary">Bi-pente de largeur 16.14m</h4>
+                </div>
+          <div class="img-wrapper">
+            <a href="../../assets/img/tole-fibro/zoom_bp16.gif"><img src="../../assets/img/tole-fibro/zoom_bp16.gif" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-plus-circle" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12 mx-auto image">
+        <div class="card-body" style="text-align: center;">
+                    <h4 class="card-title mt-5 text-center text-secondary">Bi-pente de largeur 20.02m</h4>
+                </div>
+          <div class="img-wrapper">
+            <a href="../../assets/img/tole-fibro/zoom_bp20.gif"><img src="../../assets/img/tole-fibro/zoom_bp20.gif" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-plus-circle" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12 mx-auto image">
+        <div class="card-body" style="text-align: center;">
+                    <h4 class="card-title mt-5 text-center text-secondary">Bi-pente de largeur 23.89m</h4>
+                </div>
+          <div class="img-wrapper">
+            <a href="../../assets/img/tole-fibro/zoom_bp24.gif"><img src="../../assets/img/tole-fibro/zoom_bp24.gif" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-plus-circle" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12 mx-auto image">
+        <div class="card-body" style="text-align: center;">
+                    <h4 class="card-title mt-5 text-center text-secondary">Bi-pente de largeur 27.77m</h4>
+                </div>
+          <div class="img-wrapper">
+            <a href="../../assets/img/tole-fibro/zoom_bp28.gif"><img src="../../assets/img/tole-fibro/zoom_bp28.gif" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-plus-circle" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+      </div><!-- End row -->
+    </div><!-- End image gallery -->
+  </div><!-- End container --> 
+</section>
+
+</div>
+
+<a class="text-secondary" href="../couverture-tole.php" ><h3 class=" ms-5 mt-4 fs-5 fw-semibold">Retour au catalogue de couverture tôle</h3></a>
+<a id="button" class="text-decoration-none"></a>
+<?php include '../../assets/include/footer-couv.php' ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+</script>
+<script src="../../assets/js/gallerie.js"></script>
+<script src="../../assets/js/back.js"></script>
+</body>
